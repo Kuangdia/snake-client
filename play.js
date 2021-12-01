f8d22f4e974744dcd721067fd5d0cd9a07a20014
Diana <slorrrr@gmail.com>
2021-12-01 17:23:49 -0500

    connect to snek server

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "192.168.2.34",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Connected");
+  })
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
