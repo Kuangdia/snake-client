7f90b8b36e1e7049d6a09c9e3dca486773fb67b9
Diana <slorrrr@gmail.com>
2021-12-01 17:44:42 -0500

    Writing back to server on connection

--- client.js
+++ client.js
@@ -1,4 +1,9 @@
 const net = require("net");
+const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin
+// });
 
 // establishes a connection with the game server
 const connect = function () {
@@ -10,10 +15,19 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // On connect
   conn.on('connect', () => {
-    console.log("Connected");
+    // check that we're connected to server
+    console.log("You are connected");
+    // writes to server
+    conn.write("Name: XXH")
   })
 
+  // rl.on('line', (input) => {
+  //   conn.write(`${input}\n`)
+  // })
+
+  // reads data sent by server
   conn.on('data', (data) => {
     console.log(data);
   })
